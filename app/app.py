40a844ef54bb046b0a3d41d9460ca8ba5ffa55d3
assanewele <127050568+assanewele@users.noreply.github.com>
2024-11-30 13:41:28 +0000

    Update 1 app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "This is Assane Diouf's sample flask app deployed in Azure (Azure Container App) help to github action. This task is done for the cloud course lab."
+    return "This is Assane Diouf's sample flask app deployed in Azure (Azure Container App) help to github action. This task is done for the cloud course lab. Devops course."
 
 
 if __name__ == '__main__':
